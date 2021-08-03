36451f4b9b6ba8cdb529fcb1f2c1cb0fedf1f81d
michaeljgrant <mjg.grant.m@gmail.com>
2021-08-03 21:11:08 +1000

    updated styling - adding player score, text lines for already selected spaces, restart button clears scores while round end continues game and changes first player around

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ let currentPlayer = player1;
 let text = document.getElementById("textBox");
 const gameboard = ["", "", "", "", "", "", "", "", ""];
 const restartBtn = document.getElementById("restart");
-
+const warningText = document.getElementById("warning");
 const physicalBoard = document.querySelectorAll(".gameboard");
-const testPhys = document.querySelectorAll(".spaceOnBoard");
+const boardSpaces = document.querySelectorAll(".spaceOnBoard");
+
+
 const spaceSelected = (event) => {
     let selectedIndex = event.target.dataset.index;
-    console.log("Space has been selected");
+    if (event.target.innerText){
+      warningText.innerText = "That space is already selected";
+      setTimeout(()=>{
+        warningText.innerText = "";
+      }, 1500);
+      return;
+    }
     event.target.innerText = currentPlayer;
     gameboard[selectedIndex] = event.target.innerText;
     if (playerWon()) {
         text.innerText = `${currentPlayer} has won!`;
-        restart();
+        endOfRound();
         return;
       }
   
@@ -28,12 +36,14 @@ for (space of physicalBoard) {
     space.addEventListener('click', spaceSelected);
 }
 
+const scoreboardX = document.getElementById("playerX");
+const scoreboardO = document.getElementById("playerO");
 
 
 const playerWon = () => {
     if (gameboard[0] === currentPlayer) {
       if (gameboard[1] === currentPlayer && gameboard[2] === currentPlayer) {
-        if(currentPlayer = player1){
+        if(currentPlayer === player1){
           scoreboardX.innerText ++;
         } else {
           scoreboardO.innerText ++;
@@ -41,28 +51,63 @@ const playerWon = () => {
         return true;
       }
       if (gameboard[3] === currentPlayer && gameboard[6] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
       if (gameboard[4] === currentPlayer && gameboard[8] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
     }
     if (gameboard[8] === currentPlayer) {
       if (gameboard[2] === currentPlayer && gameboard[5] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
       if (gameboard[6] === currentPlayer && gameboard[7] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
     }
     if (gameboard[4] === currentPlayer) {
       if (gameboard[1] === currentPlayer && gameboard[7] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
       if (gameboard[3] === currentPlayer && gameboard[5] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
       if (gameboard[2] === currentPlayer && gameboard[6] === currentPlayer) {
+        if(currentPlayer === player1){
+          scoreboardX.innerText ++;
+        } else {
+          scoreboardO.innerText ++;
+        }
         return true;
       }
     }
@@ -78,19 +123,33 @@ const playerDraw = () => {
       restart();
     }
   };
+ 
+const endOfRound = () => {
+  setTimeout(() => {
+    gameboard.forEach((space, i) => {
+      gameboard[i] = "";
+    });
+    for (spaces of boardSpaces){
+        spaces.innerText = "";
+    }
+    text.innerText = "";
+    currentPlayer = currentPlayer === player2 ? player1 : player2;
+      }, 1500);
+}
 
 
 const restart = () => {
-    setTimeout(() => {
+  
       gameboard.forEach((space, i) => {
         gameboard[i] = "";
       });
-      for (spaces of testPhys){
+      for (spaces of boardSpaces){
           spaces.innerText = "";
       }
       text.innerText = "";
       currentPlayer = currentPlayer === player2 ? player1 : player2;
-    }, 3000);
-  };
+      scoreboardO.innerText = "0";
+      scoreboardX.innerText = "0";
+    }
 
 restartBtn.addEventListener('click', restart);
